3764e85f5d6b945291775bd73b174701f3b40b46
Michael Wong <mizik_jah@yahoo.com>
2022-08-15 15:43:18 -0400

    finished ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = (array, ...removeElements) => {
+  for (let i = 0; i < removeElements.length; i++) {
+    if (removeElements[i] && array.includes(removeElements[i])) {
+      const result = array.indexOf(removeElements[i]);
+      array.splice(result, 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
