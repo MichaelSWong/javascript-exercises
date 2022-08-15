a799ba9b135267739e971fd749b9d031fe6509bd
Michael Wong <mizik_jah@yahoo.com>
2022-08-15 16:04:29 -0400

    finished ex 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = (temp) => {
+  let converstion = (temp - 32) * (5 / 9);
+  return Math.round(converstion * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = (temp) => {
+  let converstion = temp * (9 / 5) + 32;
+  return Math.round(converstion * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
