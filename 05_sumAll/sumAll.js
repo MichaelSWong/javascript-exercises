32aa7e06e4184540d8c25c8b7ac473a8a8b0bb76
Michael Wong <mizik_jah@yahoo.com>
2022-08-15 15:51:27 -0400

    finshed ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = (num1, num2) => {
+  if (
+    typeof num1 !== 'number' ||
+    typeof num2 !== 'number' ||
+    num1 < 1 ||
+    num2 < 1
+  )
+    return 'ERROR';
+  let total = 0;
+  let min = Math.min(num1, num2);
+  let max = Math.max(num1, num2);
+  for (let i = min; i <= max; i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
