0cb4f8376afe9597d96af55b297f73a67639e358
Michael Wong <mizik_jah@yahoo.com>
2022-08-15 14:41:51 -0400

    TDD exercies 1 to 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = (str, num) => {
+  if (str === '') return '';
+  if (num < 0) return 'ERROR';
 
+  let result = '';
+  for (let i = 1; i <= num; i++) {
+    result += str;
+  }
+  console.log({ result });
+  return result;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let result = '';
+  const test = Array.from(str);
+  for (let i = test.length - 1; i >= 0; --i) {
+    result += test[i];
+  }
+  return result;
 };
 
 // Do not edit below this line
