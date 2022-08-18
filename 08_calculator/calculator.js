0a0664c0ca00a2273def5e8b65d40559092fb3fd
Michael Wong <WongM@fnb-corp.com>
2022-08-18 12:56:20 -0400

    finished ex 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = (num1, num2) => {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = (num1, num2) => {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = (numArray) => {
+  if (numArray.length === 0) return 0;
+  return numArray.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    0
+  );
 };
 
-const multiply = function() {
-
+const multiply = (numArray) => {
+  if (numArray.length === 0) return 0;
+  return numArray.reduce(
+    (previousValue, currentValue) => previousValue * currentValue,
+    1
+  );
 };
 
-const power = function() {
-	
+const power = (num1, num2) => {
+  return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = (num) => {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  if (num === 2) return num;
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +41,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
